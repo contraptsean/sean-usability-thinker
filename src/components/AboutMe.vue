00220e707c2230781e9c780c51f0f70b82efc120
Sean Hogan <sean.hogan@csuglobal.edu>
2022-03-31 15:46:40 -0400

    highlighter

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -5,7 +5,7 @@
     <div class="row align-items-center g-lg-5 py-5">
       <div class="col-lg-7 text-center text-lg-start">
         <h1 class="">Sean Hogan</h1>
-        <p class="col-lg-10 fs-4"><span class="text-decoration-underline">Usability Thinker</span> who specializes in big messes, funny analogies, and solving problems for people.</p>
+        <p class="col-lg-10 fs-4"><span class="highlight-container"><span class="highlight">Usability Thinker</span></span> who specializes in big messes, funny analogies, and solving problems for people.</p>
       </div>
       <div class="col-md-10 mx-auto col-lg-5">
         <img class="w-100" src="@/assets/undraw_thought_process.svg" />
@@ -16,7 +16,38 @@
 </template>
 <style scoped>
 .text-decoration-underline {
-
   text-decoration-color: #00BFA6!important;
 }
+.highlight-container, .highlight {
+  position: relative;
+}
+.highlight-container{
+
+  display: inline-block;
+}
+.highlight-container:before,
+.highlight-container:after{
+    content: ' ';
+    display: block;
+    height: 90%;
+    width: 100%;
+    margin-left: -3px;
+    margin-right: -3px;
+    position: absolute;
+}
+.highlight-container:before{
+    background: rgba(68,122,184,0.1);
+    transform: rotate(2deg);
+    top: -1px;
+    left: -1px;
+}
+  .highlight-container:after {
+    background: rgba(68,122,184,0.2);
+    top: 3px;
+    right: -2px;
+}
+  .highlight {
+    color: #333;
+    z-index: 4;
+  }
 </style>
